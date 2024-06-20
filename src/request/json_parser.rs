765a5aed9812e335956fb2fce60f0207f23c9752
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-06-20 17:52:58 +0300

    Update serde import and Deserialize trait

--- src/request/json_parser.rs
+++ src/request/json_parser.rs
@@ -1,17 +1,17 @@
 use super::HttpRequest;
 use serde::de::Error;
-use serde_json::{ Result as JsonResult, Error as JsonError, from_slice as json_from_slice };
+use serde_json::{ Result as JsonResult, Error as JsonError, from_slice };
 
-pub use serde::de::Deserialize;
+pub use serde::Deserialize;
 
 impl HttpRequest {
-    pub fn body_as_json<'a, T>(&'a self) -> JsonResult<T> where T: Deserialize<'a> {
+    pub fn json_from_struct<'a, T>(&'a self) -> JsonResult<T> where T: Deserialize<'a> {
         match self.body.as_ref() {
             None => {
                 return Err(JsonError::custom("No body"));
             }
             Some(body) => {
-                let json: T = json_from_slice(body.as_slice())?;
+                let json: T = from_slice(body.as_slice())?;
                 Ok(json)
             }
         }

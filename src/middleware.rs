9d2a461ae04f45f05f181fa93362b635c135b9f6
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-06-30 23:35:39 +0300

    Update Middleware to remove MiddlewareHandler and implement RouteHandler

--- src/middleware.rs
+++ src/middleware.rs
@@ -1,42 +1,45 @@
-use crate::{ request::HttpRequest, response::HttpResponse, server::Handler };
+use crate::{
+    request::HttpRequest,
+    response::HttpResponse,
+    server::route_handler::{ RouteHandler, HandlerResult },
+};
 
 pub mod gzip;
 
 pub use gzip::Gzip;
 
 /// Middleware trait to be implemented for creating middleware
-/// 
+///
 /// # Example
-/// 
+///
 /// ```rust
 /// use krustie::{ request::HttpRequest, response::HttpResponse, middleware::Middleware };
-/// 
+///
 /// struct AddKrustieHeader;
-/// 
+///
 /// impl AddKrustieHeader {
 ///   fn add_header(res: &mut HttpResponse) {
 ///     res.insert_header("Server", "Krustie");
 ///   }
 /// }
-/// 
+///
 /// impl Middleware for AddKrustieHeader {
-///   fn middleware(req: &HttpRequest, res: &mut HttpResponse) {
+///   fn middleware(&self, req: &HttpRequest, res: &mut HttpResponse) {
 ///     AddKrustieHeader::add_header(res);
 ///   }
 /// }
 ///
 pub trait Middleware {
-    fn middleware(req: &HttpRequest, res: &mut HttpResponse) where Self: Sized;
+    fn middleware(&self, req: &HttpRequest, res: &mut HttpResponse) -> HandlerResult;
 }
 
-pub(crate) trait MiddlewareHandler: Middleware + Handler {}
-
-// Implement the `Handler` trait for any type that implements the `Middleware` trait
-impl<T> Handler for T
-where
-    T: Middleware,
-{
-    fn handle(&self, request: &HttpRequest, response: &mut HttpResponse) {
-        T::middleware(request, response);
+impl RouteHandler for Box<dyn Middleware> {
+    fn handle(
+        &self,
+        request: &HttpRequest,
+        response: &mut HttpResponse,
+        _: &Vec<String>
+    ) -> HandlerResult {
+        self.middleware(request, response)
     }
 }

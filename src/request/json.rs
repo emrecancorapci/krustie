579570f24435aa83c378b10cc831da146fbc9d91
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-07-02 16:34:56 +0300

    Add get_body_as_json function doctest

--- src/request/json.rs
+++ src/request/json.rs
@@ -2,6 +2,31 @@ use std::io::{ Error, ErrorKind };
 use serde_json::{ from_slice, Value };
 
 impl super::HttpRequest {
+    /// Get the body of the request as a JSON object
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use krustie::{ HttpResponse, StatusCode, HttpRequest, json::{ json, get_string_from_json } };
+    ///
+    ///fn post_req(req: &HttpRequest, res: &mut HttpResponse) {
+    ///    match req.get_body_as_json() {
+    ///        Ok(body) => {
+    ///            let server_key_option = body.get("server");
+    ///            if get_string_from_json(server_key_option).unwrap() == "Krustie" {
+    ///                res.status(StatusCode::Ok).json_body(body);
+    ///            } else {
+    ///                res.status(StatusCode::try_from(201).unwrap()).json_body(
+    ///                    json!({"error": "Invalid server"})
+    ///                );
+    ///            }
+    ///        }
+    ///        Err(_) => {
+    ///            res.status(StatusCode::BadRequest).json_body(json!({"error": "Invalid JSON"}));
+    ///        }
+    ///    }
+    ///}
+    ///   `````
     pub fn get_body_as_json(&self) -> Result<Value, Error> {
         match self.body.as_ref() {
             None => {
